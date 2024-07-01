3e3c948c75d745591b0affb60dfdac7c35ac3b86
Anzelah <jepkoechanzellah@gmail.com>
2024-07-01 15:59:05 +0000

    fixing index error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Use regex:re.sub to redact the password and dob values"""
     parts = message.split(separator)
-    if len(parts) > 4:
+    if len(parts) =< 5:
         ps = parts[2].split('=')[1].strip()
         dob = parts[3].split('=')[1].strip()
         return re.sub(ps, redaction, re.sub(dob, redaction, message))
