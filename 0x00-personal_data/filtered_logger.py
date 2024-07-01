73b801b1eb196b5c411ba797e2bce6ad2a310ca5
Anzelah <jepkoechanzellah@gmail.com>
2024-07-01 15:46:30 +0000

    use strip to remove any whitespaces

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,8 +8,8 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Use regex:re.sub to redact the password and dob values"""
-    ps = message.split(';')[2].split('=')[1]
-    dob = message.split(';')[3].split('=')[1]
-
+    parts = message.split(separator)
+    ps = parts[2].split('=')[1].strip()
+    dob = parts[3].split('=')[1].strip()
     res = re.sub(ps, redaction, re.sub(dob, redaction, message))
     return res
