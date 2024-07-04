61dae06babc88775df13b0c66208b1b9b87f10f5
Anzelah <jepkoechanzellah@gmail.com>
2024-07-04 15:14:38 +0000

    needed to use regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,7 +9,8 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Use regex:re.sub to redact the password and dob values"""
     parts = message.split(separator)
-    for i in range(5):
-        ps = parts[2].split('=')[1].strip()
-        dob = parts[3].split('=')[1].strip()
-        return re.sub(ps, redaction, re.sub(dob, redaction, message))
+        
+    ps = parts[2].split('=')[1].strip()
+    dob = parts[3].split('=')[1].strip()
+
+    return re.sub(ps, redaction, re.sub(dob, redaction, message)) 
