e2c71e4f9a8606e70d7012e4057deece5ad876dd
Anzelah <jepkoechanzellah@gmail.com>
2024-07-08 19:15:02 +0000

    try to edit the main.py file instead

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -8,7 +8,7 @@ get_db = __import__('filtered_logger').get_db
 db = get_db()
 cursor = db.cursor()
 cursor.execute("SELECT COUNT(*) FROM users;")
-count = cursor.fetchone()[0]
-print(count)
+for row in cursor:
+    print(row[1])
 cursor.close()
 db.close()
