a2f7e02f4788693a6d08aca0a909b934fdc44cd6
Anzelah <jepkoechanzellah@gmail.com>
2024-07-09 18:07:02 +0000

    encrypt password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Encrypt passwords"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """
+    This function expects one string argument(the password);
+    and returns a salted, hashed password, which is a byte string.
+
+    Use the bcrypt package to perform the hashing.
+    """
+    hashed = bcrypt.hashpw(b'password', bcrypt.gensalt())
+    return hashed
