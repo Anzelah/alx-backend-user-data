68205b17c2ece88fc88d66bd121b09701c471623
Anzelah <jepkoechanzellah@gmail.com>
2024-07-10 20:47:55 +0000

    add a forbidden 403 error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> None:
       - raise a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - raise a 403 error by using abort
+    """
+    abort(403)
