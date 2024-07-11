40ed65c412aaf933b67f9be88569083dcbba8dcb
Anzelah <jepkoechanzellah@gmail.com>
2024-07-11 14:51:52 +0000

    create an auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Auth class"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """A class to manage the API authentication
+    """
+    def __init__(self):
+        """Initialize instances of the class"""
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        A public method for authentication
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Public method for basic authenticatioon
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """A public method for identifying the current user
+        """
+        return None
