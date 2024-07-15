d8e80df8491bdb018172992616285f398a4204ba
Anzelah <jepkoechanzellah@gmail.com>
2024-07-15 08:55:27 +0000

    first trial for basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Inherits the Auth class"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """A class which inherits form the auth class"""
+    pass
