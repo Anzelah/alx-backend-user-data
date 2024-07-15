581840fb3910a8ff13bcda16b69d9649a60410c4
Anzelah <jepkoechanzellah@gmail.com>
2024-07-15 11:39:19 +0000

    decode the base64 encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Inherits the Auth class"""
 
 from api.v1.auth.auth import Auth
+import re
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -14,7 +16,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """Return the Base64 part of the
+        """Return the encoded Base64 part of the
         Authorization header for a Basic Authentication
         """
         if authorization_header is None:
@@ -25,3 +27,26 @@ class BasicAuth(Auth):
             return None
         encoding = authorization_header[5:].strip()
         return encoding
+
+    def decode_base64_authorization_header(self, base64_
+                                           authorization_header: str) -> str:
+        """Returns the decoded value of a
+        Base64 string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            # Check if encoded-string is a valid base64 strings
+            pattern =
+            r'^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$'
+            if not re.match(pattern, base64_authorization_header):
+                return None
+            # Decode encoded string to bytes then decode to string
+            base64_bytes = b64decode(base64_authorization_header)
+            decoded_str = base64_bytes.decode('utf-8')
+
+            return decoded_str
+        except Exception as err:
+            return str(err)
