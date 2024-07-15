df9be44e625cc2c9715e73a84a9add621d634acc
Anzelah <jepkoechanzellah@gmail.com>
2024-07-15 14:38:11 +0000

    extract user and email from decoded base64 value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,3 +50,17 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception as err:
             return str(err)
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """Extracts and returns the user email
+        and password from the Base64 decoded value.
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) != str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(':')
+        return tuple(credentials)
