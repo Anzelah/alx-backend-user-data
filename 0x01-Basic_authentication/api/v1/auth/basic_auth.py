1d1ab52b818d309b2d3d9c17971dfa50a9c01fa3
Anzelah <jepkoechanzellah@gmail.com>
2024-07-15 09:23:41 +0000

    first trial to extraccting base64 encoded auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,5 +5,21 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """A class which inherits form the auth class"""
-    pass
+    """Processes the basis authentication scheme using base64 encoding
+    """
+    def __init__(self):
+        """Initialize instances of the class
+        """
+        pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Return the Base64 part of the Authorization header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith('Basic'):
+            return None
+        encoding = authorization_header[5:].strip()
+        return encoding
