0f2ea6d2d46344508a26b5882949165ce0ac7186
Anzelah <jepkoechanzellah@gmail.com>
2024-07-16 19:22:36 +0000

    remove existing check and only have ne, otherwise youll get unauthorized instead of forbidden

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,16 +42,15 @@ def before_request():
     that is handled by function of that blueprint.
     """
     if auth is None:
-        pass
+        return
     if not auth.require_auth(request.path, excluded_paths):
-        pass
+        return
 
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None and
+       auth.session_cookie(request) is None):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
-        abort(401)
 
     res = auth.current_user(request)
     request.current_user = res
