368ac83b8511de0b208d4620458ee78d326fadd3
Anzelah <jepkoechanzellah@gmail.com>
2024-07-16 19:09:59 +0000

    fourth trial

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,15 +42,15 @@ def before_request():
     that is handled by function of that blueprint.
     """
     if auth is None:
-        return
+        pass
     if not auth.require_auth(request.path, excluded_paths):
-        return
+        pass
 
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     res = auth.current_user(request)
