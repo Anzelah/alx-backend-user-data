236e498382af87eb7ca84bf1c0080ba713726471
Anzelah <jepkoechanzellah@gmail.com>
2024-07-16 17:52:14 +0000

    get the user_id from session_id using get

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,7 @@ from uuid import uuid4
 class SessionAuth(Auth):
     """A class that implements the session-based authentication
     """
-    user_id_by_session_id = {}
+    user_id_by_session_id = {} # maps session id to user id
     # SessionAuth1.user_id_by_session_id()
 
     def __init__(self):
@@ -28,3 +28,11 @@ class SessionAuth(Auth):
                 self.session_id: user_id
                 }
         return self.session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return a user's id based on the session id
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        userId = self.user_id_by_session_id.get(session_id)
+        return userId
