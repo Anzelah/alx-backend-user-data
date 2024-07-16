0692a5526f08591b97e14783f46deac139894508
Anzelah <jepkoechanzellah@gmail.com>
2024-07-16 15:21:12 +0000

    create an empty class sessionAuth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Session-based authetication"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """A class that implements the session-based authentication
+    """
+    pass
