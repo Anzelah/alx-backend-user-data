85fc4defe8f5ea72b31089626fb752658e2cca93
Anzelah <jepkoechanzellah@gmail.com>
2024-07-20 19:21:04 +0000

    use the right return type annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> object:
+    def add_user(self, email: str, hashed_password: str) -> user.User:
         """This method returns a user object
         """
         sess = self._session
