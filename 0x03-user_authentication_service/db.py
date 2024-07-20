ecdfb0a4ebcee247a6983f2b98f46bfb8a35addd
Anzelah <jepkoechanzellah@gmail.com>
2024-07-20 19:08:37 +0000

    dont forget to annotate

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> Obj:
         """This method returns a user object
         """
         sess = self._session
