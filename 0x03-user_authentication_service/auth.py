e775b30b9bab542a0438b77fd1176c577417d345
Anzelah <jepkoechanzellah@gmail.com>
2024-07-24 18:09:13 +0000

    fix syntax error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,6 +83,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(id=user_id)
-            self._db.update_user(user.id, user.session_id=None)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
