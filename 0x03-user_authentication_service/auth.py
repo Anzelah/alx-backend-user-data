8147216b4ab67c08bf34f1f2b3db7f6d51653daa
Anzelah <jepkoechanzellah@gmail.com>
2024-07-24 18:14:39 +0000

    trying task 12 again

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,9 +62,9 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return None
-        sessionId = _generate_uuid()
-        self._db.update_user(user.id, session_id=sessionId)
-        return sessionId
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """Returns the corresponding User or None accordint to the sessionId
@@ -74,9 +74,9 @@ class Auth:
 
         try:
             user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-        return user
 
     def destroy_session(self, user_id: int) -> None:
         """Updates the corresponding user’s session ID to None
