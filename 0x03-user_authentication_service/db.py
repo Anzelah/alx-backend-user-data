36e9219495a40beac99c83a7c1b091f0644c857e
Anzelah <jepkoechanzellah@gmail.com>
2024-07-24 17:00:48 +0000

    correct the find user by function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,15 +48,14 @@ class DB:
         """Takes in arbitrary keyword arguments and returns the first row found
         in the users table as filtered by the method’s input arguments.
         """
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound
-            return user
-        except NoResultFound:
-            raise
-        except InvalidRequestError:
-            raise
+        for key, value in kwargs.items():
+            if value is None or not isinstance(key, str):
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
