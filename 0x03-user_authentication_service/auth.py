0064bb26f281b30b78045d6ca1e983beb47a7d84
Anzelah <jepkoechanzellah@gmail.com>
2024-07-25 08:57:33 +0000

    raise valueerror if reset_token doesnt exist

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,8 +103,8 @@ class Auth:
         password and update the user’s hashed_password field with
         the new hashed password and reset_token field to None. Returns None
         """
-        user = self._db.find_user_by(reset_token=reset_token)
-        if not user:
+        if not reset_token:
             raise ValueError
+        user = self._db.find_user_by(reset_token=reset_token)
         hashed = _hash_password(password)
         self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
